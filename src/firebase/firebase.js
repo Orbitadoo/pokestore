a468320de203707657ad6aecc96cd538598095d9
Orbitadoo <juanpaavaldezz@gmail.com>
2023-05-15 21:11:09 -0300

    first-commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMqtgCp-okZqe5vezVovdKFb-PlYBC0Mw",
+  authDomain: "pokestore-cd808.firebaseapp.com",
+  projectId: "pokestore-cd808",
+  storageBucket: "pokestore-cd808.appspot.com",
+  messagingSenderId: "629061430702",
+  appId: "1:629061430702:web:1d06a794fe1d99dc832c3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
